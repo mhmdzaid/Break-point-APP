c11a2f82c90356957736898bb0b0913fef50204e
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-07-10 14:15:09 -0700

    creating the project and setting up the firebase

---
+++ Pods/Target Support Files/Pods-breakPointApp/Pods-breakPointApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_breakPointApp : NSObject
+@end
+@implementation PodsDummy_Pods_breakPointApp
+@end
---
+++ breakPointApp/controller/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  breakPointApp
+//
+//  Created by mohamed zead on 7/10/18.
+//  Copyright © 2018 zead. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
