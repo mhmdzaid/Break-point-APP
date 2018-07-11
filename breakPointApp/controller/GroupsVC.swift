a7574650e3542e176cae00ba172935d1516db441
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-07-11 08:57:30 -0700

    designing the feedVC and GroupVC

--- breakPointApp/controller/GroupsVC.swift
+++ breakPointApp/controller/GroupsVC.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class GroupsVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
