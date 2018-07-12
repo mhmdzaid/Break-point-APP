027e36b86e4b241f08a0fcc8b2269cd059971aac
mohamed zead <sayedabdo@Sayeds-Mac.local>
2018-07-11 17:01:20 -0700

    adding MeVC design

---
+++ breakPointApp/controller/MeVC.swift
@@ -0,0 +1,25 @@
+//
+//  MeVC.swift
+//  breakPointApp
+//
+//  Created by mohamed zead on 7/11/18.
+//  Copyright © 2018 zead. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var ProfileImage: UIImageView!
+    @IBOutlet weak var EmailLbl: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+    @IBAction func SignOutBtnWasPressed(_ sender: Any) {
+    }
+}
